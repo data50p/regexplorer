cb7c4404f67d97bc5b1aa08c6310b12a86d55580
Lars Nordberg <lars@femtioprocent.se>
2013-06-23 16:43:11 +0200

    removed autogenerated file

--- src/main/java/com/femtioprocent/regexplorer/Version.java
+++
@@ -1,7 +0,0 @@
-package com.femtioprocent.regexplorer;
-	    public class Version {
-	    public static final String projectGroup = "com.femtioprocent";
-	    public static final String projectName = "regexplorer";
-	    public static final String projectVersion = "1.0.1";
-	    public static final String mavenBuildTimestamp = "20130623-0129";
-	    }
